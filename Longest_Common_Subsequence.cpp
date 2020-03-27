e0e4c5780f6e006f187361dd9d0f3f8e1e1fd5ef
Semal Sherathia <51093922+Semal10@users.noreply.github.com>
2020-03-27 18:35:35 +0530

    Update Longest_Common_Subsequence.cpp

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,48 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include<climits>
 using namespace std;
 
-int main(){
-fast;
-cin.tie(0);
-string s1="abcdaf";
-string s2="acbcf";
-//vector<vector<int>> dp(s1.length()+1,s2.length()+1);
-int dp[s1.length()+1][s2.length()+1];
-int i,j,maximum=0;
-string ans="";
+typedef long long int lld;
+
+string a,b;
+string ans;
 bool flag,f;
-// Initializing 0th Row && 0th Column As 0 
-for(i=0;i<s1.length()+1;i++){
-    dp[i][0]=0;
-}
-for(i=0;i<s2.length()+1;i++){
-    dp[0][i]=0;
-}
-// Matrix Iteration Logic
-// flag & f is used to keep track for which alphabets
-// are responsible for LCS (Diagonal Traversal)
-for(i=1;i<s1.length()+1;i++){
-    flag=false;
-    f=false;
-    for(j=1;j<s2.length()+1;j++){
-        if(s1[i-1]==s2[j-1]){
-            dp[i][j]=dp[i-1][j-1]+1;
-            flag=true;
-        }
-        else{
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-        }
-        if(dp[i][j]>maximum){
-            maximum=dp[i][j];
-            f=true;
-        }
+lld dp[1001][1001],dp1[1001][1001],maxx;
+
+//TOP-DOWN
+lld lcs(lld n1,lld n2){
+    lld c=INT_MIN;
+    if(n1<0 || n2<0) return 0;
+    if(dp[n1][n2]!=-1) return dp[n1][n2];
+    if(a[n1]==b[n2]){
+        c=lcs(n1-1,n2-1)+1;
     }
-    if(flag && f){
-        ans+=s1[i-1];
+    else{
+        c=max(lcs(n1-1,n2),lcs(n1,n2-1));
     }
+    if(n1>=0 && n2>=0) dp[n1][n2]=c;
+    return c;
 }
-// Longest Common Subsequence Length
-cout<<maximum<<endl;
-// Longest Common Subsequence String
-cout<<ans<<endl;
+
+//BOTTOM-UP
+lld lcsDynamic(lld n1 , lld n2){
+    for(lld i=1;i<=n1+1;i++){
+        flag=false;
+        f=false;
+        for(lld j=1;j<=n2+1;j++){
+            if(a[i]==b[j]){
+                dp1[i][j]=dp1[i-1][j-1]+1;
+                flag=true;
+            }
+            else{
+                dp1[i][j]=max(dp1[i-1][j],dp1[i][j-1]);
+            }
+            if(dp1[i][j]>maxx){
+                f=true;
+                maxx=dp1[i][j];
+            }
+        }
+        if(flag && f) ans+=a[i-1];
+    }
+    return dp1[n1+1][n2+1];
+}
+
+
+int main() {
+	lld i,j;
+	cin>>a>>b;
+	ans="";
+	memset(dp,-1,sizeof(dp));
+	memset(dp1,0,sizeof(dp1));
+	cout<<lcsDynamic(a.length()-1,b.length()-1)<<endl;
+	cout<<ans<<endl;
+	
 }
