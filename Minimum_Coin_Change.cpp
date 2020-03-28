4471780b51ee228b46c39b874bbc45241b181011
Semal Sherathia <51093922+Semal10@users.noreply.github.com>
2020-03-28 12:36:05 +0530

    Update Minimum_Coin_Change.cpp

--- Minimum_Coin_Change.cpp
+++ Minimum_Coin_Change.cpp
@@ -1,42 +1,68 @@
-int minimumCoinChange(int amt,int coins[],int n){
-    int dp[amt+1];
-    int numbretrieve[amt+1];
-    set<int> s;
-    int i,j;
-    // BASE CASE For Making 0 Amount You Need Zero Coins
-    dp[0]=0;
-    for(i=1;i<=amt;i++){
-        dp[i]=INT_MAX;
+#include <bits/stdc++.h>
+#include<climits>
+using namespace std;
+
+#define watch(x) cout<<(#x)<<" = "<<x<<endl
+typedef long long int lld;
+
+lld a[1001],value,n,dp[1001][1001],dp1[1001];
+
+//TOP-DOWN
+lld minimumCoinChange(lld n,lld value){
+    lld c=INT_MAX;
+    if(value==0) return dp[n][value]=0;
+    if(n<0  || value<0) return INT_MAX;
+    if(dp[n][value]!=-1) return dp[n][value];
+    
+    if(n>=1 && value>=a[n-1])
+    c=min(minimumCoinChange(n-1,value),minimumCoinChange(n,value-a[n-1])+1);
+    
+    return dp[n][value]=c;
+}
+
+//TOP-DOWN (For Loop)
+lld minimumCoinChangeLoop(lld n,lld value){
+    if(value<0 || n<0) return INT_MAX;
+    if(value==0) return dp[n][value]=0;
+    if(dp[n][value]!=-1) return dp[n][value];
+    
+    lld ans=INT_MAX;
+    for(lld i=0;i<n;i++){
+        if(a[i]<=value){
+            lld curr=minimumCoinChangeLoop(n,value-a[i])+1;
+        
+            ans=min(ans,curr);
+        }
     }
-    for(i=0;i<=amt;i++){
-        numbretrieve[i]=-1;
+    return dp[n][value]=ans;
+}
+
+//BOTTOM-UP
+lld minimumCoinChangeDynamic(lld n,lld value){
+    dp1[0]=0;
+    for(lld i=1;i<=value;i++){
+        dp1[i]=INT_MAX;
     }
-    for(i=0;i<n;i++){
-        for(j=1;j<=amt;j++){
-            if(coins[i]<=j){
-                dp[j]=min(dp[j],dp[j-coins[i]]+1);
-                numbretrieve[j]=i;
+    for(lld i=0;i<n;i++){
+        for(lld j=1;j<=value;j++){
+            if(j>=a[i]){
+               dp1[j]=min(dp1[j],dp1[j-a[i]]+1); 
             }
         }
     }
-    int answer=dp[amt];
-    while(amt!=0){
-        s.insert(coins[numbretrieve[amt]]);
-        amt-=coins[numbretrieve[amt]];
-    }
-    for(auto it=s.begin();it!=s.end();it++){
-        cout<<(*it)<<" ";
-    }
-    cout<<endl;
-    return answer;
+    
+    return dp1[value];
 }
 
-int main(){
-fast;
-cin.tie(0);
-int coins[4]={1,5,6,8};
-int amt=14;
-// Minimum Coin Required To Change To amt is
-cout<<minimumCoinChange(amt,coins,4)<<endl;
+
+int main() {
+	lld i,j;
+	cin>>n;
+	for(i=0;i<n;i++){
+	    cin>>a[i];
+	}
+	memset(dp,-1,sizeof(dp));
+	cin>>value;
+	cout<<minimumCoinChangeDynamic(n,value)<<endl;
 
 }
